cf02c0fd7f057c724e037a8dcf52385accd8141c
Administrator <howmao@qq.com>
2023-12-18 09:39:04 +0800

    集合抽象类

---
+++ src/Abstracts/CollectionAbstract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Createlinux\EasyGaoDe\Abstracts;
+
+use Createlinux\EasyGaoDe\GaoDe\GeoCode\GeoCodeItem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+
+abstract class CollectionAbstract extends Collection
+{
+
+}
