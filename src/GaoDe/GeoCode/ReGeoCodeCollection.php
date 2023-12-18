cc55cf15c3dd85e3f52d49c3275f0c927eed4e69
Administrator <howmao@qq.com>
2023-12-18 14:34:41 +0800

    todo

---
+++ src/GaoDe/GeoCode/ReGeoCodeCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Createlinux\EasyGaoDe\GaoDe\GeoCode;
+
+use Createlinux\EasyGaoDe\Abstracts\CollectionAbstract;
+
+class ReGeoCodeCollection extends CollectionAbstract
+{
+
+}
--- src/GaoDe/GeoCode/ReGeoResponse.php
+++ src/GaoDe/GeoCode/ReGeoResponse.php
@@ -6,7 +6,22 @@ use Createlinux\EasyGaoDe\Http\ResponseAbstract;
 
 class ReGeoResponse extends ResponseAbstract
 {
+    /**
+     * 地理编码信息列表 结果对象列表
+     * @var ReGeoCodeCollection
+     */
+    protected ?ReGeoCodeCollection $reGeoCodes = null;
 
-    //TODO
+    /**
+     * @remark
+     * @return ReGeoCodeCollection
+     */
+    public function getReGeoCodes(): ReGeoCodeCollection
+    {
+        if ($this->reGeoCodes === null) {
+            $collection = new ReGeoCodeCollection();
+            //TODO
+        }
+    }
 
 }
