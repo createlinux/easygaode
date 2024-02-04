7c9dd8e73affbc1e98fdbdbca4eb059816e60776
Administrator <howmao@qq.com>
2024-02-04 14:03:04 +0800

    test

--- test.php
+++ test.php
@@ -1,10 +1,19 @@
 <?php
 require_once './vendor/autoload.php';
 
-$key = 'fab933e2cf9c631ad0a59f1f6dd9db93';
+$key = '20d1331c238cc91b8c1a3319b5bc8fe1';
+
 
 $easygaode = new \Createlinux\EasyGaoDe\Application($key);
 
-$geocode = $easygaode->createGeoCode();
-$geocode->setAddress();
-$geocode->setOutput();
+$regeocode = $easygaode->createReGeoCode();
+$regeocode->setLocation("113.667892,34.720432");
+$regeocode->setOutput(\Createlinux\EasyGaoDe\Enums\OutputInterface::json);
+$response = $regeocode->query();
+
+if ($response->isFailed()) {
+    return print_r($response->getInfo() . "\n");
+}
+
+$reGeoCode = $response->getReGeoCode();
+print_r($reGeoCode->getAddressComponent());
